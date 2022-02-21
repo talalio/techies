92fa34a0f0ceae0fb8780783e19ba629671d148c
talal <talaldev@protonmail.com>
2022-02-21 13:14:13 +0200

    added more fields

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,47 @@
+import axios from 'axios'
 
 const Modal = () => {
 
 	const handleSubmition = (e) => {
 		e.preventDefault()
+
+		const newUser = {
+			fname: e.target.first.value,
+			lname: e.target.last.value,
+			email: e.target.email.value,
+			picture: e.target.picture.value,
+			bio: e.target.bio.value,
+			city: e.target.city.value,
+			social: {
+				github: e.target.github.value,
+				twitter: e.target.twitter.value,
+				discord: e.target.discord.value,
+				telegram: e.target.telegram.value,
+			},
+		}
+
+		const addUser = async (user) => {
+			await axios.post('http://localhost:5000/users/add', user)
+		}
+
+		addUser(newUser)
 	}
 
 	return (
 		<form action="" onSubmit={(e) => { handleSubmition(e) }}>
-			<div className="modal" id="user-modal">
+			<div className="modal justify-center" id="user-modal">
 				<div className="modal-box grid grid-flow-row grid-cols-1 gap-3 min-w-200 sm:min-w-fit max-w-400">
-					<input type="text" className="input input-primary input-bordered input-md w-full" name="first" placeholder="First name" required/>
-					<input type="text" className="input input-primary input-bordered input-md w-full" name="last" placeholder="Last name" required/>
+					<input type="text" className="input input-primary input-bordered input-sm w-full" name="first" placeholder="First name" required/>
+					<input type="text" className="input input-primary input-bordered input-sm w-full" name="last" placeholder="Last name" required/>
+					<input type="email" className="input input-primary input-bordered input-sm w-full" name="email" placeholder="Email" required/>
+					<input type="text" className="input input-primary input-bordered input-sm w-full" name="city" placeholder="City" required/>
+					<input type="email" className="input input-primary input-bordered input-sm w-full" name="picture" placeholder="Profile picture"/>
 					<textarea className="textarea textarea-primary textarea-bordered" name="bio" placeholder="Bio"></textarea>
-						
-					<div className="modal-action grid grid-flow-col gap-2">
+					<input type="url" className="input input-primary input-bordered input-sm w-full" name="github" placeholder="Github Link" />
+					<input type="url" className="input input-primary input-bordered input-sm w-full" name="twitter" placeholder="Twitter Link" />
+					<input type="url" className="input input-primary input-bordered input-sm w-full" name="discord" placeholder="Discord Link" />
+					<input type="url" className="input input-primary input-bordered input-sm w-full" name="telegram" placeholder="Telegram Link" />
+					<div className="modal-action grid grid-flow-col gap-2 justify-center">
 						<button className="btn btn-primary btn-borderd">
 							Join
 						</button>
