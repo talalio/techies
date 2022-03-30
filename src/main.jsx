65d174dbefdbb76fc1dc6b91697bcd4aae7143d4
talal <talaldev@protonmail.com>
2022-03-30 12:28:03 -0400

    Added Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App page="Home"/>}/>
+      <Route path="login" element={<App page="Login"/>}/>
+      <Route path="register" element={<App page="Register"/>}/>
+      <Route path="rank" element={<App page="Rank"/>}/>
+    </Routes>
+  </BrowserRouter>),
   document.getElementById('root')
 )
