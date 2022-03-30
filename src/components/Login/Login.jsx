bb1f3518b877a1941f92048face7eb4e9fe81e8b
talal <talaldev@protonmail.com>
2022-03-30 14:24:20 -0400

    Added login & register page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-const Login = () => (
 
-	<React.Fragment>
-		<div>
-			
-		</div>
-	</React.Fragment>
-	
+const Login = ({onlogin}) => (
+
+	<form action="" onSubmit={onlogin} className="mt-auto flex flex-col gap-2 max-w-fit justify-center self-center items-center">
+		<input className="input input-md input-primary w-full" type="email" name="email" placeholder="Email"/>
+		<input className="input input-md input-primary w-full" type="password" name="password" placeholder="Password"/>
+		<input className="btn btn-primary w-full" type="submit" value="Login"/>
+		<button className="btn btn-secondary w-full">
+			<FontAwesomeIcon className="mr-2" icon={faGithub} size="2x"/>
+			Continue with Github
+		</button>
+	</form>
 
 )
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom'
 
-const Register = () => (
-	<div className="mt-6 border-2 border-black flex flex-row w-full h-full justify-center items-center">
-		<div className="flex flex-col gap-2">
-			<input className="input input-md input-primary" type="text" name="username" placeholder="Username"/>
-			<input className="input input-md input-primary" type="email" name="email" placeholder="Email"/>
-			<input className="input input-md input-primary" type="password" name="password" placeholder="Password"/>
-			<input className="btn btn-primary" type="submit" value="Register"/>
-		</div>
-	</div>
+const Register = ({onregister}) => (
+	<form action="" onSubmit={onregister} className="mt-auto flex flex-col gap-2 max-w-fit justify-center self-center items-center">
+		<input className="input input-md input-primary w-full" type="text" name="username" placeholder="Username"/>
+		<input className="input input-md input-primary w-full" type="email" name="email" placeholder="Email"/>
+		<input className="input input-md input-primary w-full" type="password" name="password" placeholder="Password"/>
+		<input className="btn btn-primary w-full" type="submit" value="Register"/>
+		<button className="btn btn-secondary w-full">
+			<FontAwesomeIcon className="mr-2" icon={faGithub} size="2x"/>
+			Continue with Github
+		</button>
+		<Link className="underline" to="/login">Already have an account ?</Link>
+	</form>
 )
 
 export default Register
